eb03d4944bfbf768b9fe457c3a3da4203429e246
jakkdl <h6+github@pm.me>
2022-07-11 22:19:52 +0000

    good progress, stuff is pretty close to working

--- quickstart.py
+++ quickstart.py
@@ -1,6 +1,9 @@
 """Do various discord and gsheet automation for the dominion league"""
 
 import os.path
+import asyncio
+import pickle
+from typing import TypeAlias, cast
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -8,12 +11,11 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build, Resource
 from googleapiclient.errors import HttpError
 
-import discord
-import discord.ext.commands as commands # pylint: disable=consider-using-from-import
-#import discord.ext
-#from discord.ext import commands
+import nextcord
+import nextcord.ext.commands as commands  # pylint: disable=consider-using-from-import
 
-from discord.bot import ApplicationCommandMixin
+# import nextcord.ext
+# from nextcord.ext import commands
 
 
 # If modifying these scopes, delete the file token.json.
@@ -24,7 +26,6 @@ SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 SAMPLE_SPREADSHEET_ID = "1O8-YTGYGBDFSjSReHLOmvGgBesrvMju8KEGqGWmxQpA"  # S53
 
 TIMEOUT_TRIES = 10
-
 
 
 USERS_NAME_HEADERS = ["username", "discriminator", "id"]
@@ -39,18 +40,21 @@ USERS_ROLE_HEADERS = [
     "Proper League Division",
 ]
 
+RoleList: TypeAlias = dict[int, list[str]]
 
-def get_requested_roles(sheet: Resource) -> dict[str, list[str]]:
+
+async def get_requested_roles(sheet: Resource) -> RoleList:
     """returns a dict of discord_id's and requested discord roles given a season setup sheet"""
+    print('poop')
     result = None
-    users: dict[str, list[str]] = {}
+    users: RoleList = {}
     for tries in range(TIMEOUT_TRIES):
         try:
             result = (
                 sheet.values()
                 .batchGet(
                     spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                    ranges=["Users!A2:C2", "Users!A3:C", "Users!O3:V"],
+                    ranges=["Users!A2:C2", "Users!O2:V2", "Users!A3:C", "Users!O3:V"],
                 )
                 .execute()
             )
@@ -74,42 +78,138 @@ def get_requested_roles(sheet: Resource) -> dict[str, list[str]]:
         print("No data found.")
         return {}
 
-    name_headers, names, role_headers, requested_roles = [
+    name_headers, role_headers, names, requested_roles = [
         value_ranges[i]["values"] for i in range(len(value_ranges))
     ]
+
+    name_headers = name_headers.pop()
+    role_headers = role_headers.pop()
 
     assert name_headers == USERS_NAME_HEADERS
     assert role_headers == USERS_ROLE_HEADERS
 
-    names = list(value_ranges[0]["values"])[:50]
-    requested_roles = list(value_ranges[1]["values"])
-
-    for name, roles in zip(names, requested_roles):
-        users[name] = [
+    for (_, _, d_id), roles in zip(names, requested_roles):
+        req_roles = [
             role
             for role, requested in zip(role_headers, roles)
             if (requested == "TRUE")
         ]
 
+        if not req_roles:
+            continue
+        users[int(d_id)] = req_roles
+
     return users
+
+
+def get_roles(guild: nextcord.Guild, user_id: int) -> list[str]:
+    """returns a list of roles for a given discord user"""
+    roles: list[str] = []
+    member = guild.get_member(user_id)
+    if member is None:
+        return roles
+    for role in member.roles:
+        roles.append(role.name)
+    return roles
+
+
+def mismatching_roles(
+    guild: nextcord.Guild, requested_roles: RoleList
+) -> dict[nextcord.Member, set[nextcord.Role]]:
+    def role_lookup(rolename: str) -> nextcord.Role:
+        role = roles_lookup.pop(rolename, None)
+        if role is not None:
+            return role
+        for role in guild.roles:
+            if role.name == rolename:
+                return role
+        raise ValueError("role {rolename} not found")
+
+    roles_lookup: dict[str, nextcord.Role] = {}
+    result = {}
+
+
+    for d_id, roles in requested_roles.items():
+        member = guild.get_member(d_id)
+        if member is None:
+            print(f'failed to find member {d_id}')
+            continue
+
+        parsed_roles = set(role_lookup(role) for role in roles)
+        actual_roles = set(member.roles)
+        diff_roles = parsed_roles - actual_roles
+        if not diff_roles:
+            continue
+        result[member] = parsed_roles - actual_roles
+
+    print([(f'@{m.name}#{m.discriminator}',roles) for m,roles in result.items()])
+    return result
+
 
 class MyCog(commands.Cog):
     def __init__(self, bot, sheet_resource: Resource) -> None:
         super().__init__()
         self.bot = bot
         self.sheet_resource = sheet_resource
+        self.background_tasks: set[asyncio.tasks.Task] = set()
+
+    async def my_wrapper(self, channel: nextcord.abc.Messageable):
+        req_roles = await get_requested_roles(self.sheet_resource)
+        with open("req_roles.pickle", "wb") as file:
+            pickle.dump(req_roles, file)
+        await channel.send(f"updated {len(req_roles)} entries")
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f'We have logged in as {self.bot.user}')
+        print(f"We have logged in as {self.bot.user}")
 
-    @commands.command()
-    async def hello(self, ctx, *, member: discord.Member = None):
+    @nextcord.slash_command(dm_permission=True)
+    async def hello(
+        self, interaction: nextcord.Interaction, *, member: nextcord.Member = None
+    ):
         """Says hello"""
-        member = member or ctx.author
-        await ctx.send(f'Hello {member}~')
+        hello_target = member or interaction.user
+        await interaction.send(f"Hello {hello_target}~")
 
+    @nextcord.slash_command(dm_permission=True)
+    async def addrole(
+        self,
+        interaction: nextcord.Interaction,
+        *,
+        member: nextcord.Member,
+        role: nextcord.Role,
+    ):
+        """Add role to user"""
+        await interaction.send(f"Adding {role} to {member}")
 
+    @nextcord.slash_command(dm_permission=True)
+    async def update_requested_roles(self, interaction: nextcord.Interaction):
+        """Quits"""
+        task = asyncio.create_task(
+            self.my_wrapper(cast(nextcord.abc.Messageable, interaction.channel))
+        )
+        self.background_tasks.add(task)
+        task.add_done_callback(self.background_tasks.discard)
+        await interaction.send("Running sheets update in background~")
+
+    @nextcord.slash_command(dm_permission=True)
+    async def quit(self, interaction: nextcord.Interaction):
+        """Quits"""
+        await interaction.send("Quitting~")
+        await self.bot.close()
+
+    @nextcord.slash_command(dm_permission=True)
+    async def mismatching_roles(self, interaction: nextcord.Interaction):
+        """Quits"""
+        # warn if out of date, error if missing
+        with open("req_roles.pickle", "rb") as file:
+            requested_roles = pickle.load(file)
+        dom_guild = [g for g in self.bot.guilds if g.id == 212660788786102272].pop()
+        mmmr = mismatching_roles(dom_guild, requested_roles)
+        # send members not found
+        # send members missing roles
+        #print(mmmr)
+        await interaction.send("done")
 
 
 def main() -> None:
@@ -137,16 +237,15 @@ def main() -> None:
     service: Resource = build("sheets", "v4", credentials=creds)
 
     # Call the Sheets API
-    sheet: Resource = service.spreadsheets()
+    sheet: Resource = service.spreadsheets()  # pylint: disable=no-member
 
-
-    #intents = discord.Intents.default()
-    #intents.members = True
-    #bot = MyBot(intents=intents, sheet_resource = sheet)
-    bot = commands.Bot()
+    intents = nextcord.Intents.default()
+    intents.members = True
+    # bot = MyBot(intents=intents, sheet_resource = sheet)
+    bot = commands.Bot(intents=intents)
     bot.add_cog(MyCog(bot, sheet))
 
-    with open('discord_token', encoding='utf-8') as file:
+    with open("discord_token", encoding="utf-8") as file:
         discord_token = file.readline().strip()
     bot.run(discord_token)
 
